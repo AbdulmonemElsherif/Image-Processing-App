2bd0507effeb016dab378865aacd5b8b22d2efb6
MohamedOsama190 <mohamed.dien01@eng-st.cu.edu.eg>
2024-04-22 20:28:31 +0200

    okay

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\men3em\image\project\myproject\venv
+set VIRTUAL_ENV=G:\HEM Spring 2023\Image\Task4new\Image-Processing-App\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
